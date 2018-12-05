4ecd1037aae3e5fd308a520b4091b072fd820cf1
digimon <digimon.1740@gmail.com>
2018-12-05 11:35:00 +0900

    when -> given 으로 변경

--- src/test/java/com/kakao/todoapp/core/service/user/UserServiceTests.java
+++ src/test/java/com/kakao/todoapp/core/service/user/UserServiceTests.java
@@ -16,61 +16,61 @@ import org.springframework.test.context.junit4.SpringRunner;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.when;
 
 @Slf4j
 @RunWith(SpringRunner.class)
 public class UserServiceTests {
 
-	@MockBean
-	private MessageSourceService messageSourceService;
+    @MockBean
+    private MessageSourceService messageSourceService;
 
-	@MockBean
-	private UserRepository userRepository;
+    @MockBean
+    private UserRepository userRepository;
 
-	private UserService userService;
+    private UserService userService;
 
-	private static final String USER_NAME = "user";
+    private static final String USER_NAME = "user";
 
-	@Before
-	public void setUp() {
-		userService = new UserService(userRepository, messageSourceService);
-		User actual = new User(USER_NAME, PasswordEncodeUtils.encodePassword("password"), Role.ROLE_USER);
-		when(this.userRepository.save(actual)).thenReturn(actual);
-	}
+    @Before
+    public void setUp() {
+        userService = new UserService(userRepository, messageSourceService);
+    }
 
-	@Test
-	public void findByNameTest() {
-		given(this.userRepository.findByName(USER_NAME))
-			.willReturn(new User(USER_NAME, PasswordEncodeUtils.encodePassword("password"), Role.ROLE_USER));
+    @Test
+    public void findByNameTest() {
+        given(this.userRepository.findByName(USER_NAME))
+                .willReturn(new User(USER_NAME, PasswordEncodeUtils.encodePassword("password"), Role.ROLE_USER));
 
-		User actual = userService.findByName(USER_NAME);
+        User actual = userService.findByName(USER_NAME);
 
-		assertThat(actual).isNotNull();
-		assertThat(actual.getName()).isEqualTo(USER_NAME);
-		assertThat(actual.getRole()).isEqualTo(Role.ROLE_USER);
+        assertThat(actual).isNotNull();
+        assertThat(actual.getName()).isEqualTo(USER_NAME);
+        assertThat(actual.getRole()).isEqualTo(Role.ROLE_USER);
 
-		log.info("actual : {}", actual);
-	}
+        log.info("actual : {}", actual);
+    }
 
-	@Test
-	public void findByNameOrElseThrowTest() {
-		given(this.userRepository.findByName("none"))
-			.willReturn(new User("none", PasswordEncodeUtils.encodePassword("password"), Role.ROLE_USER));
+    @Test
+    public void findByNameOrElseThrowTest() {
+        given(this.userRepository.findByName("none"))
+                .willReturn(new User("none", PasswordEncodeUtils.encodePassword("password"), Role.ROLE_USER));
 
-		assertThatThrownBy(() -> userService.findByNameOrElseThrow(USER_NAME))
-			.isInstanceOf(ResourceNotFoundException.class);
-	}
+        assertThatThrownBy(() -> userService.findByNameOrElseThrow(USER_NAME))
+                .isInstanceOf(ResourceNotFoundException.class);
+    }
 
-	@Test
-	public void createTest() {
-		User user = userService.create(new User(USER_NAME, "password", Role.ROLE_USER));
+    @Test
+    public void createTest() {
+        User actual = new User(USER_NAME, PasswordEncodeUtils.encodePassword("password"), Role.ROLE_USER);
+        given(this.userRepository.save(actual)).willReturn(actual);
 
-		assertThat(user).isNotNull();
-		assertThat(user.getName()).isEqualTo(USER_NAME);
-		assertThat(user.getRole()).isEqualTo(Role.ROLE_USER);
+        User user = userService.create(new User(USER_NAME, "password", Role.ROLE_USER));
 
-		log.info("user : {}", user);
-	}
+        assertThat(user).isNotNull();
+        assertThat(user.getName()).isEqualTo(USER_NAME);
+        assertThat(user.getRole()).isEqualTo(Role.ROLE_USER);
+
+        log.info("user : {}", user);
+    }
 
 }
