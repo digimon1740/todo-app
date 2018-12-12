0033ff3ad3c872032b3513b66ec733814f2d429a
digimon <digimon.1740@gmail.com>
2018-12-12 18:29:29 +0900

    HTTP status 코드 수정

--- src/main/java/com/kakao/todoapp/web/board/BoardController.java
+++ src/main/java/com/kakao/todoapp/web/board/BoardController.java
@@ -45,35 +45,35 @@ public class BoardController {
 	ResponseEntity<BoardDto> create(Principal principal, @RequestBody BoardDto boardDto) {
 		User user = userService.findByPrincipal(principal);
 		boardService.create(modelMapperUtils.convertToEntity(boardDto, Board.class), user);
-		return new ResponseEntity<>(boardDto, HttpStatus.OK);
+		return new ResponseEntity<>(boardDto, HttpStatus.CREATED);
 	}
 
 	@PutMapping(value = "/{id}")
 	ResponseEntity<BoardDto> modify(Principal principal, @RequestBody BoardDto boardDto) {
 		User user = userService.findByPrincipal(principal);
 		boardService.modify(modelMapperUtils.convertToEntity(boardDto, Board.class), user.getId());
-		return new ResponseEntity<>(boardDto, HttpStatus.OK);
+		return new ResponseEntity<>(boardDto, HttpStatus.ACCEPTED);
 	}
 
 	@PutMapping(value = "/{id}/up")
 	ResponseEntity<Void> up(Principal principal, @PathVariable("id") Long id) {
 		User user = userService.findByPrincipal(principal);
 		boardService.up(id, user.getId());
-		return new ResponseEntity<>(HttpStatus.OK);
+		return new ResponseEntity<>(HttpStatus.ACCEPTED);
 	}
 
 	@PutMapping(value = "/{id}/down")
 	ResponseEntity<Void> down(Principal principal, @PathVariable("id") Long id) {
 		User user = userService.findByPrincipal(principal);
 		boardService.down(id, user.getId());
-		return new ResponseEntity<>(HttpStatus.OK);
+		return new ResponseEntity<>(HttpStatus.ACCEPTED);
 	}
 
 	@DeleteMapping(value = "/{id}")
 	ResponseEntity<Void> delete(Principal principal, @PathVariable("id") Long id) {
 		User user = userService.findByPrincipal(principal);
 		boardService.delete(id, user.getId());
-		return new ResponseEntity<>(HttpStatus.OK);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 
 
