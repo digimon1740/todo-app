8944fc8f4a0b38dca992c3ff48a793d60112b9b2
digimon <digimon.1740@gmail.com>
2018-12-13 15:04:31 +0900

    super type token 적용

--- src/main/java/com/kakao/todoapp/utils/mapper/ModelMapperUtils.java
+++ src/main/java/com/kakao/todoapp/utils/mapper/ModelMapperUtils.java
@@ -5,24 +5,36 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.Type;
+
 @Slf4j
 @Component
 public class ModelMapperUtils {
 
-	private ModelMapper modelMapper;
+    private ModelMapper modelMapper;
 
-	@Autowired
-	private ModelMapperUtils(ModelMapper modelMapper) {
-		this.modelMapper = modelMapper;
-	}
+    @Autowired
+    private ModelMapperUtils(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
 
-	public <T> T convertToDto(Object entity, Class<T> dto) {
-		T returnDto = modelMapper.map(entity, dto);
-		return returnDto;
-	}
+    public <T> T convertToDto(Object entity, Class<T> dto) {
+        T returnDto = modelMapper.map(entity, dto);
+        return returnDto;
+    }
 
-	public <T> T convertToEntity(Object dto, Class<T> entity) {
-		T returnEntity = modelMapper.map(dto, entity);
-		return returnEntity;
-	}
+    public <T> T convertToDto(Object entity, Type dto) {
+        T returnDto = modelMapper.map(entity, dto);
+        return returnDto;
+    }
+
+    public <T> T convertToEntity(Object dto, Class<T> entity) {
+        T returnEntity = modelMapper.map(dto, entity);
+        return returnEntity;
+    }
+
+    public <T> T convertToEntity(Object dto, Type entity) {
+        T returnEntity = modelMapper.map(dto, entity);
+        return returnEntity;
+    }
 }
--- src/main/java/com/kakao/todoapp/web/board/BoardController.java
+++ src/main/java/com/kakao/todoapp/web/board/BoardController.java
@@ -6,10 +6,18 @@ import com.kakao.todoapp.core.dto.board.BoardDto;
 import com.kakao.todoapp.core.service.board.BoardService;
 import com.kakao.todoapp.core.service.user.UserService;
 import com.kakao.todoapp.utils.mapper.ModelMapperUtils;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 import java.util.List;
@@ -19,62 +27,63 @@ import java.util.List;
 @RequestMapping(path = "/boards")
 public class BoardController {
 
-	private UserService userService;
+    private UserService userService;
 
-	private BoardService boardService;
+    private BoardService boardService;
 
-	private ModelMapperUtils modelMapperUtils;
+    private ModelMapperUtils modelMapperUtils;
 
-	@Autowired
-	public BoardController(UserService userService,
-	                       BoardService boardService,
-	                       ModelMapperUtils modelMapperUtils) {
-		this.userService = userService;
-		this.boardService = boardService;
-		this.modelMapperUtils = modelMapperUtils;
-	}
+    @Autowired
+    public BoardController(UserService userService,
+                           BoardService boardService,
+                           ModelMapperUtils modelMapperUtils) {
+        this.userService = userService;
+        this.boardService = boardService;
+        this.modelMapperUtils = modelMapperUtils;
+    }
 
-	@GetMapping
-	public ResponseEntity<List<?>> findAll(Principal principal) {
-		User user = userService.findByPrincipal(principal);
-		List<?> boards = modelMapperUtils.convertToDto(boardService.findAllByUserIdOrderByOrdinal(user.getId()), List.class);
-		return new ResponseEntity<>(boards, HttpStatus.OK);
-	}
+    @GetMapping
+    public ResponseEntity<List<?>> findAll(Principal principal) {
+        User user = userService.findByPrincipal(principal);
+        List<Board> boards = modelMapperUtils.convertToDto(boardService.findAllByUserIdOrderByOrdinal(user.getId()),
+                                                           new TypeToken<List<Board>>() {}.getType());
+        return new ResponseEntity<>(boards, HttpStatus.OK);
+    }
 
-	@PostMapping
-	ResponseEntity<BoardDto> create(Principal principal, @RequestBody BoardDto boardDto) {
-		User user = userService.findByPrincipal(principal);
-		boardService.create(modelMapperUtils.convertToEntity(boardDto, Board.class), user);
-		return new ResponseEntity<>(boardDto, HttpStatus.CREATED);
-	}
+    @PostMapping
+    ResponseEntity<BoardDto> create(Principal principal, @RequestBody BoardDto boardDto) {
+        User user = userService.findByPrincipal(principal);
+        boardService.create(modelMapperUtils.convertToEntity(boardDto, Board.class), user);
+        return new ResponseEntity<>(boardDto, HttpStatus.CREATED);
+    }
 
-	@PutMapping(value = "/{id}")
-	ResponseEntity<BoardDto> modify(Principal principal, @RequestBody BoardDto boardDto) {
-		User user = userService.findByPrincipal(principal);
-		boardService.modify(modelMapperUtils.convertToEntity(boardDto, Board.class), user.getId());
-		return new ResponseEntity<>(boardDto, HttpStatus.ACCEPTED);
-	}
+    @PutMapping(value = "/{id}")
+    ResponseEntity<BoardDto> modify(Principal principal, @RequestBody BoardDto boardDto) {
+        User user = userService.findByPrincipal(principal);
+        boardService.modify(modelMapperUtils.convertToEntity(boardDto, Board.class), user.getId());
+        return new ResponseEntity<>(boardDto, HttpStatus.ACCEPTED);
+    }
 
-	@PutMapping(value = "/{id}/up")
-	ResponseEntity<Void> up(Principal principal, @PathVariable("id") Long id) {
-		User user = userService.findByPrincipal(principal);
-		boardService.up(id, user.getId());
-		return new ResponseEntity<>(HttpStatus.ACCEPTED);
-	}
+    @PutMapping(value = "/{id}/up")
+    ResponseEntity<Void> up(Principal principal, @PathVariable("id") Long id) {
+        User user = userService.findByPrincipal(principal);
+        boardService.up(id, user.getId());
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
 
-	@PutMapping(value = "/{id}/down")
-	ResponseEntity<Void> down(Principal principal, @PathVariable("id") Long id) {
-		User user = userService.findByPrincipal(principal);
-		boardService.down(id, user.getId());
-		return new ResponseEntity<>(HttpStatus.ACCEPTED);
-	}
+    @PutMapping(value = "/{id}/down")
+    ResponseEntity<Void> down(Principal principal, @PathVariable("id") Long id) {
+        User user = userService.findByPrincipal(principal);
+        boardService.down(id, user.getId());
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
 
-	@DeleteMapping(value = "/{id}")
-	ResponseEntity<Void> delete(Principal principal, @PathVariable("id") Long id) {
-		User user = userService.findByPrincipal(principal);
-		boardService.delete(id, user.getId());
-		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-	}
+    @DeleteMapping(value = "/{id}")
+    ResponseEntity<Void> delete(Principal principal, @PathVariable("id") Long id) {
+        User user = userService.findByPrincipal(principal);
+        boardService.delete(id, user.getId());
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 
 
 }
